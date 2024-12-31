1440caa1ad3621f8b38176d527cdf871bdfcdcfa
SatishYadav2003 <sy389698@gmail.com>
2024-12-31 23:16:25 +0530

    remove schedule

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import time
 import os
-import schedule
+# import schedule
 
 def execute_movie_scrapper_fun():
     os.system("python movie_scrapper.py")
-    
-
-schedule.every(1).minutes.do(execute_movie_scrapper_fun)
 
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+execute_movie_scrapper_fun()    
+
+# schedule.every(1).minutes.do(execute_movie_scrapper_fun)
+
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
 
 
